58f6c69fddbaab7000c444b1629c1e32fff0f017
Anderson Humberto Meneghesso <meneander@gmail.com>
2020-12-04 22:01:22 -0300

    Versao final da aula de 04/12/2020

---
+++ APIProdutos/Controllers/TipoEnderecoController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIProdutos.Models;
+using GITDAO;
+using GITDAO.Entity.Cliente;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIProdutos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoEnderecoController : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        public ActionResult addTipoEndereco(TipoEndereco tipoEndereco)
+        {
+            DAOTipoEndereco daoTipoEndereco = new DAOTipoEndereco();
+            int intChave = daoTipoEndereco.AddRecord(tipoEndereco.descricao);
+
+            if (intChave > 0)
+                return Created("", intChave);
+            else
+                return BadRequest();
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult getTipoEndereco()
+        {
+            List<TipoEnderecoDTO> objRetorno = new List<TipoEnderecoDTO>();
+            DAOTipoEndereco daoTipoEndereco = new DAOTipoEndereco();
+            List<TipoEnderecoModel> lstTipoEndereco = daoTipoEndereco.GetRecord();
+
+            foreach (TipoEnderecoModel item in lstTipoEndereco)
+                objRetorno.Add(new TipoEnderecoDTO()
+                {
+                    ID = item.ID,
+                    Descricao = item.Descricao
+                });
+
+            if (lstTipoEndereco?.Count > 0)
+                return Ok(lstTipoEndereco);
+            else
+                return NotFound();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult getTipoEndereco(int id)
+        {
+            TipoEnderecoDTO objRetorno = new TipoEnderecoDTO();
+            DAOTipoEndereco daoTipoEndereco = new DAOTipoEndereco();
+            List<TipoEnderecoModel> lstTipoEndereco = daoTipoEndereco.GetRecord(id);
+
+            if (lstTipoEndereco?.Count > 0)
+            {
+                objRetorno.ID = lstTipoEndereco[0].ID;
+                objRetorno.Descricao = lstTipoEndereco[0].Descricao;
+
+                return Ok(objRetorno);
+            }    
+            else
+                return NotFound();
+        }
+
+        public class TipoEndereco
+        {
+            public string descricao { get; set; }
+        }
+    }
+}
--- GITDAO/DAO/Cliente/DAOTipoEndereco.cs
+++ GITDAO/DAO/Cliente/DAOTipoEndereco.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using GITDAO.Commons.Providers;
 using GITDAO.Entity.Cliente;
 using MySql.Data.MySqlClient;
@@ -8,7 +9,24 @@ namespace GITDAO
 {
     public class DAOTipoEndereco
     {
-        public List<TipoEnderecoModel> GetRecord()
+        public int AddRecord(string descricao)
+        {
+            using (MySqlConnection conn = new MySqlConnection(ConfigurationServiceProvider.GetConnectionString("GTI.MySQL")))
+            {
+                conn.Open();
+
+                StringBuilder stbComando = new StringBuilder();
+                stbComando.Append("Insert into TipoEndereco (descricao) ");
+                stbComando.Append($" values ('{descricao}')");
+
+                using (MySqlCommand command = new MySqlCommand(stbComando.ToString(), conn))
+                {
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<TipoEnderecoModel> GetRecord(int? id = null)
         {
             List<TipoEnderecoModel> objRetorno = new List<TipoEnderecoModel>();
 
@@ -16,7 +34,20 @@ namespace GITDAO
             {
                 conn.Open();
 
-                using (MySqlCommand command = new MySqlCommand("Select ID, descricao from TipoEndereco", conn))
+                // EXEMPLO DO QUE "NÃO SE DEVE FAZER".... CONCATENAÇÃO "DESENFREADA" DE STRINGS....
+                //string strComando = "";
+                //strComando += "Select ID";             ''  / 'Select ID'
+                //strComando += " , descricao";           ''  / 'Select ID' / 'Select ID , descricao'
+                //strComando += "From TipoEndereco ";
+
+                StringBuilder stbComando = new StringBuilder();
+                stbComando.Append("Select ID");
+                stbComando.Append(" , descricao ");
+                stbComando.Append("From TipoEndereco ");
+                if (id != null)
+                    stbComando.Append(" Where ID = " + id.ToString());
+
+                using (MySqlCommand command = new MySqlCommand(stbComando.ToString(), conn))
                 {
                     using (MySqlDataReader mySqlData = command.ExecuteReader())
                     {
